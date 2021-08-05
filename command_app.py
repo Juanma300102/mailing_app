01b03851df71e98b59b95188e916db2ce940cdfc
Juan Martin Pedrozo <pedrozo.juanma@gmail.com>
2021-08-05 10:09:49 -0300

    continue_by feature added

--- command_app.py
+++ command_app.py
@@ -1,13 +1,13 @@
 import os.path
 from sys import path
 
-from MAILING_APP.common_services import *
+from common_services import *
 
 def main():
     mailingBox = MailingService('smtp.gmail.com', 465)
-    mailingBox.logIn('recepcionpoloticmisiones@gmail.com', 'polotic30')
+    mailingBox.logIn('cursospoloticmisiones@gmail.com', 'ultraTicStar32')
 
-    csvPath = '/home/juanma/Escritorio/TRABAJO/2021/CURSOS/DESARROLLO WEB FULLSTACK CON JAVA/INSCRIPTOS_POLO.csv' # os.path.join(os.getcwd(),'tests_mails.csv')
+    csvPath = '/home/juanma/Escritorio/TRABAJO/2021/ADVA/TALLERES/inscriptos_taller3_Adva.csv' # os.path.join(os.getcwd(),'tests_mails.csv')
     csvLoader = CsvLoader(csvPath, 'r', ['correo', 'nombre', 'apellido', 'dni', 'pdf'])
 
     recipients = csvLoader.getContentAsList(firstLineHeaders=True)
@@ -15,13 +15,13 @@ def main():
     # recipients.append(['pedrozo.juanma@gmail.com', 'Juan Martin', 'Pedrozo',/home/juanma/Escritorio/PYTHON/Proyectos_Polo/tests_mails.csv' '43944733', ''])
 
     # recipients = ''
-    subject = 'Información - Curso de Desarrollo Web Fullstack con Java'
+    subject = 'Taller 3 - Programación de Videojuegos'
     from_= 'Equipo Polo Tic'
     _content = 'Buen dia ${nombre}\n\n' \
                'Te comunicamos desde el Polo TIC que debido a que no confirmaste la asistencia mediante el correo que te mandamos a tu casilla de mensajes, <b>has perdido la vacante</b> al Taller de Diseño de Videojuego.'                        
-    _footer = ''
+    _footer = 'Equipo Polo Tic'
     
-    template = os.path.join(os.getcwd(), 'MAILING_APP', 'Templates', 'CURSOS', 'JAVA', 'template_original.html')
+    template = os.path.join(os.getcwd(), 'Templates', 'ADVA', 'ADVA_TALLER_3.html')
 
     mailingBox.clasifyAndMakeSendMails(subject=subject,
                                        from_=from_,
@@ -30,7 +30,7 @@ def main():
                                        content=_content,
                                        multiple_recipients=True,
                                        footer=_footer,
-                                       continue_in=2574) # TERMINADO
+                                       continue_in=23) 
     csvLoader.close()
     mailingBox.conn.close()
 
