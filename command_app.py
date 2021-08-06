d98c662f6348c71938c800bba3ac401cf7943cbe
Juan Martin Pedrozo <pedrozo.juanma@gmail.com>
2021-08-06 12:32:46 -0300

    Envio de correos de Java - Aviso fusion

--- command_app.py
+++ command_app.py
@@ -5,22 +5,22 @@ from common_services import *
 
 def main():
     mailingBox = MailingService('smtp.gmail.com', 465)
-    mailingBox.logIn('cursospoloticmisiones@gmail.com', 'ultraTicStar32')
+    mailingBox.logIn('recepcionpoloticmisiones@gmail.com', 'polotic30')
 
-    csvPath = os.path.join(os.getcwd(),'intro_inscriptosxlsx.csv')
+    csvPath = '/home/juanma/Escritorio/TRABAJO/2021/CURSOS/JAVA EDICION 2/INSCRIPTOS_JAVA.csv' #os.path.join(os.getcwd(),'tests_mails.csv')
     csvLoader = CsvLoader(csvPath, 'r', ['correo', 'nombre', 'apellido', 'dni', 'pdf'])
 
     recipients = csvLoader.getContentAsList(firstLineHeaders=True)
-    # recipients.append(['polotic.equipopedagogico@gmail.com', 'Julieta', 'Baez', '', ''])
+    # recipients.append(['comunicacionpoloticmisiones@gmail.com', 'Florencia', '', '', ''])
     # recipients.append(['pedrozo.juanma@gmail.com', 'Juan Martin', 'Pedrozo',/home/juanma/Escritorio/PYTHON/Proyectos_Polo/tests_mails.csv' '43944733', ''])
 
     # recipients = ''
-    subject = 'Curso de Introducción a la Programacion'
+    subject = 'Primer Clase Desarrollo con JAVA - Fusión Silicon Misiones'
     from_= 'Equipo Polo Tic'
     _content = ''                        
     _footer = 'Equipo Polo Tic'
     
-    template = os.path.join(os.getcwd(), 'Templates', 'CURSOS', 'introduccion_programacion', 'template_original.html')
+    template = os.path.join(os.getcwd(), 'Templates', 'CURSOS', 'JAVA', 'AVISO_DE_FUSION.html')
 
     mailingBox.clasifyAndMakeSendMails(subject=subject,
                                        from_=from_,
@@ -29,7 +29,7 @@ def main():
                                        content=_content,
                                        multiple_recipients=True,
                                        footer=_footer,
-                                       continue_in=0) 
+                                       continue_in=1191) #TODO seguir con envio de correos de java
     csvLoader.close()
     mailingBox.conn.close()
 
